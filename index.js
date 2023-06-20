c43d410818761fe98fa94f7b483067e89e1829e7
lovely boiii <94507951+Santoshkunwar44@users.noreply.github.com>
2023-06-20 04:37:17 -0700

    Update index.js
    origin added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(
 
 cors({
-    origin:"http://localhost:3000",
+    origin:["http://localhost:3000","http://localhost:5173"],
     methods:["GET","PUT","POST","DELETE","OPTIONS"],
     credentials:true
 })
